192702db1df7da06961152503c368e66d4550ad0
jgonzalez <jota_q91@hotmail.com>
2017-02-25 09:13:58 -0500

    cambio en el archivo de rutas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hola/index', 'HolaController@index');
+Route::get('hola/saludo/{nombre}', 'HolaController@saludar');
+
